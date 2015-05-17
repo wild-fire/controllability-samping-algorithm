b2e93f05c311b62ff289d3172cd81adcbfaa92b9
David Brenes <davidjbrenes@gmail.com>
2015-05-17 18:02:10 +0200

    Changing args for the job so we need less

--- src/jobs/SamplingAlgorithm.java
+++ src/jobs/SamplingAlgorithm.java
@@ -22,8 +22,8 @@ public class SamplingAlgorithm {
 	public static void main(String[] args) throws Exception  {
 		Configuration conf = new Configuration();
 		conf.set("com.wildfire.graph_file_path", args[0]);
-		conf.set("com.wildfire.mms_file_path", args[1]);
-		conf.set("com.wildfire.removal_nodes_file_path", args[2]);
+		conf.set("com.wildfire.mms_file_path", args[1] + "/mms.csv");
+		conf.set("com.wildfire.removal_nodes_file_path", args[1] + "/removal-nodes.csv");
 		
 		Job job = Job.getInstance(conf, "controllability sampling algorithm");
 		
@@ -39,8 +39,8 @@ public class SamplingAlgorithm {
 		job.setReducerClass(LongSumReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(LongWritable.class);
-	    FileInputFormat.addInputPath(job, new Path(args[2]));
-		FileOutputFormat.setOutputPath(job, new Path(args[3]));
+	    FileInputFormat.setInputPaths(job, args[1] + "/removal-nodes.csv");
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 
 	}
