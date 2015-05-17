1314098986a0417b7688fe3acadd502d5440ecac
David Brenes <davidjbrenes@gmail.com>
2015-05-17 15:06:01 +0200

    Job structured around regular input
    We perform the first MMS calculation before the job is started

--- src/jobs/SamplingAlgorithm.java
+++ src/jobs/SamplingAlgorithm.java
@@ -1,6 +1,6 @@
 package jobs;
 
-import input.GraphInputFormat;
+import input.GraphReader;
 import mappers.SamplingMapper;
 
 import org.apache.hadoop.conf.Configuration;
@@ -12,20 +12,32 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.reduce.LongSumReducer;
 
+import vos.Graph;
+import vos.MMS;
+import writers.AlternativeMMSWriter;
+
 public class SamplingAlgorithm {
 	
 	public static void main(String[] args) throws Exception  {
 		Configuration conf = new Configuration();
 		conf.set("com.wildfire.graph_file_path", args[0]);
+		conf.set("com.wildfire.alternative_mms_file_path", args[1]);
+		
 		Job job = Job.getInstance(conf, "controllability sampling algorithm");
+		
+
+		// First, we read the graph and get the original MMS
+		Graph graph = GraphReader.read(job);
+		MMS mms = graph.getMMS();
+		AlternativeMMSWriter.write(mms, job);
 		job.setJarByClass(SamplingAlgorithm.class);
-		job.setInputFormatClass(GraphInputFormat.class);
 		job.setMapperClass(SamplingMapper.class);
 		job.setCombinerClass(LongSumReducer.class);
 		job.setReducerClass(LongSumReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(LongWritable.class);
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	    FileInputFormat.addInputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 
 	}
