f0993e8530e922b8ec73e88500c73cee66372f0d
David Brenes <davidjbrenes@gmail.com>
2015-05-17 18:05:26 +0200

    Removing more debug prints

--- src/mappers/SamplingMapper.java
+++ src/mappers/SamplingMapper.java
@@ -21,45 +21,41 @@ public class SamplingMapper extends Mapper<Object, Text, Text, LongWritable> {
 	private Text unmatchedNode = new Text();
 	private Graph graph;
 	private MMS mms;
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void map(Object nothing, Text removalNode,
 			Mapper<Object, Text, Text, LongWritable>.Context context)
 			throws IOException, InterruptedException {
-		// we must read the MMS and Graph file each time as we modify them each mapping run 
+		// we must read the MMS and Graph file each time as we modify them each mapping run
 		try {
 			this.graph = GraphReader.read(context);
 			this.mms = MMSReader.read(this.graph, context);
 		} catch (URISyntaxException e) {
 			throw new IOException(e.getMessage());
 		}
-		
+
 		// We prepare the array where we are going to save the configurations of unmatched nodes
 		ArrayList<Set<String>> alternativeConfigurations = new ArrayList<Set<String>>();
-		// And the variable to get a random one 
+		// And the variable to get a random one
 		Random random = new Random();
-		
-		// And prepare the first removal node (the one decided randomly by the RemovalNodesWriter and delivered by Hadoop) 
+
+		// And prepare the first removal node (the one decided randomly by the RemovalNodesWriter and delivered by Hadoop)
 		String nodeToRemove = removalNode.toString();
-		
-		System.out.println(">> " + nodeToRemove);
-		System.out.println(">>>> " + this.mms.getUnmatchedNodes());
-		
+
+
 		// While we can remove nodes
 		while(this.mms.removeNode(nodeToRemove)) {
 			// We store the unmatched nodes as a driver node configuration
 			alternativeConfigurations.add((Set<String>) this.mms.getUnmatchedNodes().clone());
-			System.out.println(">>>> " + this.mms.getUnmatchedNodes());
 			// And we get the matched nodes to obtain the next random removal node
 			ArrayList<String> matchedNodes = new ArrayList<String>(mms.getMatchedNodes());
 			nodeToRemove = matchedNodes.get(random.nextInt(matchedNodes.size()));
 		}
-		
+
 		// Now we get a random alternative configuration (if there's any)
 		if(!alternativeConfigurations.isEmpty()) {
 			for(String unmatchedNode : alternativeConfigurations.get(random.nextInt(alternativeConfigurations.size()))) {
-				System.out.println(">>>>>> " + unmatchedNode);
 				this.unmatchedNode.set(unmatchedNode);
 				context.write(this.unmatchedNode, one);
 			}
